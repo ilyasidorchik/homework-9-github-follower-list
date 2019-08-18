cfde063fe24b460bf525bc62e6c4e7c08c76eabe
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-18 19:07:25 +0300

    Adds Router

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -3,3 +3,30 @@
 // Роутер должен иметь роуты для компонентов Login и Search
 // Вам потребуется использовать PrivateRoute для Search
 // По умолчанию нужно перенаправлять на страницу логина
+
+import React, { PureComponent } from 'react';
+import { BrowserRouter, Switch, Redirect, Route, withRouter } from 'react-router-dom';
+
+import Login from '../Login';
+import Search from '../Search';
+import PrivateRoute from '../PrivateRoute';
+
+import styles from './Router.module.css';
+
+class Router extends PureComponent {
+    render() {
+        return (
+            <div class={styles.root}>
+                <BrowserRouter>
+                    <Switch>
+                    <Route path='/' component={Login} exact />
+                    <PrivateRoute path='/search' component={Search} />
+                    <Redirect to='/' />
+                </Switch>
+                </BrowserRouter>
+            </div>
+        );
+    }
+}
+
+export default withRouter(Router);
