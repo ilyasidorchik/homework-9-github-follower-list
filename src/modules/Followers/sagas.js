8c5f1ad573c44168822bd6f64e16231d11182d9e
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-19 01:26:02 +0300

    Adds Followers

--- src/modules/Followers/sagas.js
+++ src/modules/Followers/sagas.js
@@ -1,13 +1,25 @@
-import { takeLatest, select, put, call, fork } from 'redux-saga/effects';
+import { takeLatest, select, call, put, fork } from 'redux-saga/effects';
 
+import { fetchRequest, fetchSuccess, fetchFailure } from './actions';
+import { getApiKey } from '../Auth';
+import { getFollowersInfo } from './api';
 
 function* fetchFollowersWatcher() {
-  yield takeLatest(?, fetchFollowersFlow); // Замените вопросительный знак на подходящий экшен
+  yield takeLatest(fetchRequest, fetchFollowersFlow);
 }
 
 export function* fetchFollowersFlow(action) {
   // Реализуйте загрузку данных
   // Используйте экшены FETCH_SUCCESS / FETCH_FAILURE
+  const apiKey = yield select(getApiKey);
+
+  try {
+    const result = yield call(getFollowersInfo, apiKey, action.paylod);
+    yield put(fetchSuccess(result));
+  }
+  catch (error) {
+    yield put(fetchFailure(error));
+  }
 }
 
 export default function*() {
