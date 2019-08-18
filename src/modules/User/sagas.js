5735b96caabd57c574dcacbf9b49e73249b9426c
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-18 23:54:00 +0300

    Adds User

--- src/modules/User/sagas.js
+++ src/modules/User/sagas.js
@@ -1,14 +1,24 @@
 import { takeLatest, select, put, call, fork } from 'redux-saga/effects';
 
+import { fetchRequest, fetchSuccess, fetchFailure } from './actions';
+import { getApiKey } from '../Auth';
+import { getUserInfo } from './api';
 
 function* fetchUserWatcher() {
-  yield takeLatest(?, fetchUserFlow);
+  yield takeLatest(fetchRequest, fetchUserFlow);
 }
 
 export function* fetchUserFlow(action) {
-
+  const apiKey = yield select(getApiKey);
+  try {    
+    const result = yield call(getUserInfo, apiKey, action.payload);
+    yield put(fetchSuccess(result));
+  }
+  catch (error) {
+    yield put(fetchFailure(error));
+  }
 }
 
 export default function*() {
-
+  yield fork(fetchUserWatcher);
 }
