ec69ae36cd1b41897b9fab4dc7d2ed87d1ae8e69
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-19 02:13:05 +0300

    Adds UserInfo

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,19 +1,35 @@
 import React, { PureComponent } from 'react';
-import styles from './UserInfo.module.css';
-
 import { connect } from 'react-redux';
+
+import { getIsLoading, getData } from '../../modules/User';
+import styles from './UserInfo.module.css';
 
 class UserInfo extends PureComponent {
   render() {
     // Покажите статус загрузки
     // Если данные не были загружены - сообщите об этом пользователю
+    const { isLoading, data } = this.props;
+    
+    if (isLoading) return <div>Загрузка информации о пользователе</div>;
+    if (!data) return <div>Информация о пользователе не найдена</div>;
+
+    const { avatar_url: image, name, bio } = data;
     return (
       <div className={styles.root}>
-        {/* Отобразите данные о пользователе */}
+        <div className={styles.imageWrapper}>
+          <img src={image} alt={name} />
+        </div>
+        <div>
+          <p className='t-user-name'>{name}</p>
+          <p className='t-user-bio'>{bio}</p>
+        </div>
       </div>
     );
   }
 }
 
 // Используйте поля data, isLoading из стейта
-export default connect(state => ({}))(UserInfo);
+export default connect(state => ({
+  isLoading: getIsLoading(state),
+  data: getData(state)
+}))(UserInfo);
