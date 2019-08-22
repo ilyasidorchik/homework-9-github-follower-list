870e23e4eabf6fa9d11ceefb4687ebb276a9d470
Ilya Sidorchik <ilya@sidorchik.ru>
2019-08-22 21:58:05 +0300

    Fixes bug

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -5,28 +5,22 @@
 // По умолчанию нужно перенаправлять на страницу логина
 
 import React, { PureComponent } from 'react';
-import { BrowserRouter, Switch, Redirect, Route, withRouter } from 'react-router-dom';
+import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom';
 
 import Login from '../Login';
 import Search from '../Search';
 import PrivateRoute from '../PrivateRoute';
 
-import styles from './Router.module.css';
-
-class Router extends PureComponent {
+export default class Router extends PureComponent {
     render() {
         return (
-            <div class={styles.root}>
-                <BrowserRouter>
-                    <Switch>
+            <BrowserRouter>
+                <Switch>
                     <Route path='/' component={Login} exact />
                     <PrivateRoute path='/search' component={Search} />
                     <Redirect to='/' />
                 </Switch>
-                </BrowserRouter>
-            </div>
+            </BrowserRouter>
         );
     }
 }
-
-export default withRouter(Router);
--- src/store.js
+++ src/store.js
@@ -13,7 +13,7 @@ const createAppStore = () => {
     rootReducer,
     compose(
       applyMiddleware(sagaMiddleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+      (window.__REDUX_DEVTOOLS_EXTENSION__) ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
     )
   );
   
